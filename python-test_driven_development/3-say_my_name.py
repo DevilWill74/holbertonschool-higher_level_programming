1c4127d9024fe5ed8a724b0ca8d12a23a275bd1d
root <alexis.Fauqueur@outlook.fr>
2024-11-19 12:02:50 +0000

    Fixes say_my_name function output and tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -20,6 +20,8 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    # Construction de la sortie sans espace supplémentaire
-    full_name = f"{first_name} {last_name} ".strip()
-    print(f"My name is {full_name} ")
+    # Construction de la sortie avec un espace uniquement si nécessaire
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
