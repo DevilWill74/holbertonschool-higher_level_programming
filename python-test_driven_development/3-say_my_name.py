d75472f5892c1c895a1ebe7bcfd51da2341b8653
root <alexis.Fauqueur@outlook.fr>
2024-11-19 10:15:26 +0000

    Fix space issue in say_my_name output

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -21,8 +21,5 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    # Suppression des espaces inutiles
-    if last_name:
-        print(f"My name is {first_name} {last_name}")
-    else:
-        print(f"My name is {first_name}")
+    # Gérer un espace final attendu
+    print(f"My name is {first_name} {last_name}".strip() + " ")
