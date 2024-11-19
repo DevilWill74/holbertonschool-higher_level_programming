2b8236f10a2737b501566a41579afd6d43b49bf9
root <alexis.Fauqueur@outlook.fr>
2024-11-19 00:30:17 +0000

    Fix uppercase function with one loop and proper formatting

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,9 @@
 def uppercase(str):
     result = ""
     for char in str:
+        # Convertir en majuscule si c'est une lettre minuscule
         if ord(char) >= 97 and ord(char) <= 122:
             result += chr(ord(char) - 32)
         else:
             result += char
-    print(result)
+    print("{}".format(result))
