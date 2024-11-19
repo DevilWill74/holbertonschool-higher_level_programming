85ff1dfee784d8bfda9c69ba74169abc88f99675
root <alexis.Fauqueur@outlook.fr>
2024-11-19 09:11:44 +0000

    Correction PEP 8 pour 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,25 +3,29 @@
 This module provides the add_integer function.
 """
 
+
 def add_integer(a, b=98):
     """
-    Adds two integers or floats.
+    Adds two integers.
     If a or b is a float, it is cast to an integer before addition.
-    Raises:
-        TypeError: If a or b are not integers or floats.
-        ValueError: If a or b are NaN values.
-        OverflowError: If a or b are infinite values.
+    Raises a TypeError if a or b are not integers or floats.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    if a != a or b != b:  # Check for NaN
+    # Vérification pour les float spéciaux (NaN ou inf)
+    if a != a or b != b:  # Vérifie si `a` ou `b` est NaN
         raise ValueError("cannot convert float NaN to integer")
-    if a in [float('inf'), float('-inf')] or b in [float('inf'), float('-inf')]:
+    if (
+        a in [float('inf'), float('-inf')]
+        or b in [float('inf'), float('-inf')]
+    ):
         raise OverflowError("cannot convert float infinity to integer")
 
+    # Convertir les floats en int avant addition
     a = int(a)
     b = int(b)
+
     return a + b
