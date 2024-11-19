5ad65e33df2b3fdc9a96b29d81fb11f70b59e04a
root <alexis.Fauqueur@outlook.fr>
2024-11-19 11:39:05 +0000

    Add say_my_name function and tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-Module that defines a function to print full name
+This module contains a function that prints a formatted string
+based on first_name and last_name.
 """
+
 
 def say_my_name(first_name, last_name=""):
     """
-    Function that prints 'My name is <first_name> <last_name>'
+    Prints "My name is <first_name> <last_name>".
+
     Args:
-        first_name: first name string
-        last_name: last name string (optional)
+        first_name (str): The first name.
+        last_name (str): The last name, optional.
+
     Raises:
-        TypeError: If first_name or last_name is not a string
+        TypeError: If first_name or last_name is not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    # Supprime l'espace superflu à la fin en ajustant la gestion des chaînes
-    if last_name:
-        print("My name is {} {} ".format(first_name, last_name))
-    else:
-        print("My name is {}".format(first_name))
+    print(f"My name is {first_name} {last_name}")
