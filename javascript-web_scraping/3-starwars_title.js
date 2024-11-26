6bbdc8f27e27e72508a9fdb084d82cba84f7dd2b
root <alexis.fauqueur@outlook.fr>
2024-11-26 12:49:49 +0000

    test

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
+// script that prints the title of a Star Wars movie where the episode number
+// matches a given integer
+const movId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movId;
+const request = require('request');
 
-const axios = require('axios').default;
-const id = process.argv.slice(2)[0];
-axios.get('https://swapi-api.hbtn.io/api/films/' + id)
-  .then(function (response) {
-    console.log(response.data.title);
-  });
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
