2fb61690a39920280770587e53e863cf604839c6
root <alexis.fauqueur@outlook.fr>
2024-11-26 10:14:42 +0000

    test

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-// Write a script that reads and prints the content of a file
-const file = process.argv[2];
-const fs = require('fs');
 
-if (file) {
-  // Asynchronous version of fs.readFile. Returns the contents of the filename
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    } else console.log(data);
-  });
-}  
+const file = process.argv.slice(2)[0];
+const fs = require('fs');
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
